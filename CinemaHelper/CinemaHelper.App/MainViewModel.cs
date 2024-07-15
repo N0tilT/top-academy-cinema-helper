7b3cde84a5d7a1ba74224175baa6647d905e0a64
Timofey Latypov <90103780+N0tilT@users.noreply.github.com>
2024-07-15 20:38:37 +0300

    WPF example added

---
+++ CinemaHelper/CinemaHelper.App/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using CinemaHelper.App.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaHelper.App
+{
+    public class MainViewModel : ObservableObject
+    {
+        private string _input = string.Empty;
+        public string Input {
+            get => _input; 
+            set { 
+                _input = value; 
+                OnPropertyChanged("Input");
+            } 
+        }
+
+        private string _output = string.Empty;
+        public string Output
+        {
+            get => _output;
+            set
+            {
+                _output = value;
+                OnPropertyChanged("Output");
+            }
+        }
+
+        private RelayCommand showCommand;
+        public RelayCommand ShowCommand
+        {
+            get
+            {
+                return showCommand ??
+                  (showCommand = new RelayCommand(obj =>
+                  {
+                      Output = Input;
+                  }));
+            }
+        }
+
+        private RelayCommand clearCommand;
+        public RelayCommand ClearCommand
+        {
+            get
+            {
+                return clearCommand ??
+                  (clearCommand = new RelayCommand(obj =>
+                  {
+                      Output = string.Empty;
+                      Input = string.Empty;
+                  }));
+            }
+        }
+
+    }
+}
---
+++ CinemaHelper/CinemaHelper.App/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CinemaHelper.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainViewModel viewModel = new MainViewModel();
+        public MainWindow()
+        {
+            DataContext = viewModel;
+            InitializeComponent();
+        }
+    }
+}
