8c18c4d1e50b2639d2da419723a890c7e916ceeb
Timofey Latypov <90103780+N0tilT@users.noreply.github.com>
2024-07-19 21:08:10 +0300

    Update BookingViewModel.cs

--- CinemaHelper/CinemaHelper.App/BookingViewModel.cs
+++ CinemaHelper/CinemaHelper.App/BookingViewModel.cs
@@ -1,10 +1,12 @@
-﻿namespace CinemaHelper.App
+﻿using CinemaHelper.App.Core;
+
+namespace CinemaHelper.App
 {
-    public class BookingViewModel
+    public class BookingViewModel : ObservableObject
     {
         private TicketService ticketService;
 
-        public BookingViewModel(TicketService ticketService)
+        public BookingViewModel(TicketService ticketService) 
         {
             this.ticketService = ticketService;
         }
